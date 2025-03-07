9fa0ea2dc3e1f531c99f193fe5eaa61596a90e99
Johan Naizu <johan@naizu.in>
2025-03-08 01:42:43 +0530

    minor

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,41 +1,43 @@
-import { FC, ReactNode } from "react"
-import ParticleJs from "components/Particle"
-import { Container } from "@chakra-ui/react"
-import Helper from "./Help"
-import { useRouter } from "next/router"
+import { FC, ReactNode } from "react";
+import ParticleJs from "components/Particle";
+import { Container } from "@chakra-ui/react";
+import Helper from "./Help";
+import { useRouter } from "next/router";
 const Layout: FC<Props> = (props) => {
-  const adminRoutes = ["/admin2022", "/ca"]
-  const router = useRouter()
+  const adminRoutes = ["/admin2022", "/ca"];
+  const router = useRouter();
   return (
-    <Container >
-      <div style={{
-        position: "absolute",
-        top: 0,
-        left: 0,
-        width: "100%",
-        height: "100%",
-        zIndex: -1,
-      }}>
+    <Container>
+      <div
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          zIndex: -1,
+        }}
+      >
         {/* {!adminRoutes.includes(router.asPath) ? <ParticleJs /> : null} */}
         <ParticleJs />
       </div>
       {props.children}
-      <div style={{
-        height: "5rem"
-      }} ></div>
+      <div
+        style={{
+          height: "5rem",
+        }}
+      ></div>
       <div
         style={{
           display: "flex",
-          justifyContent: "center"
-        }}>
-        {router.asPath !== "/admin2022" ? <Helper /> : null}
-
-      </div>
+          justifyContent: "center",
+        }}
+      ></div>
     </Container>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
 interface Props {
-  children: ReactNode
+  children: ReactNode;
 }
